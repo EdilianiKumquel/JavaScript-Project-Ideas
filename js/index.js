72355192df99084d5dc9cfd1a5317cc5a6f558c5
EdilianiKumquel <kumquelediliani@gmail.com>
2021-07-24 21:39:37 -0430

    section 2, images slider automatic

--- js/index.js
+++ js/index.js
@@ -16,4 +16,27 @@ valueBtn.addEventListener("click", function(){
 	}
 	
 })
-		
+
+//section 2
+let i = 0;
+let images = ["./img/img1.jpg", 
+			  "./img/img2.jpg", 
+			  "./img/img3.jpg", 
+			  "./img/img4.jpg",
+			  "./img/img5.jpg"]
+let time = 3000;
+let sliderEl = document.getElementById("slider-el")
+
+function changeImg(img){
+	//document.slide.src = img[i];
+	if (i < img.length) {
+		sliderEl.innerHTML = `<img src="${img[i]}">`
+		i++;
+	} else {
+		i = 0;
+	}
+
+	setTimeout("changeImg(images)", time)
+}
+
+changeImg(images)
